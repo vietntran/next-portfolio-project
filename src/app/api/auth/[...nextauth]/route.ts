95071d74f5fb253642fef4d259c1f0e5f48feeb6
Viet Tran <vietnamtran@gmail.com>
2025-01-07 15:10:37 +0000

    Fix type issues

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 // src/app/api/auth/[...nextauth]/route.ts
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth/next";
+import authOptions from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
